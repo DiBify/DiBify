4406bbb96d7fd4846bad83a63dd2baf1441133af
XAKEPEHOK <icq-475204@ya.ru>
2022-11-01 00:59:15 +0300

    [New] ModelHelper::indexBy()

--- src/Helpers/ModelHelper.php
+++ src/Helpers/ModelHelper.php
@@ -23,18 +23,25 @@ class ModelHelper
      */
     public static function indexById(ModelInterface ...$models): array
     {
-        $indexed = [];
-        foreach ($models as $model) {
-
+        $indexed = self::indexBy(function (ModelInterface $model) {
             if (!$model->id()->isAssigned()) {
                 throw new NotPermanentIdException('Array of models can not be indexed by non-permanent id');
             }
-
-            $indexed["{$model->id()}"] = $model;
-        }
+            return (string)$model->id();
+        }, ...$models);
 
         if (count($models) !== count($indexed)) {
             throw new DuplicateModelException('Few models in array has same id');
+        }
+
+        return $indexed;
+    }
+
+    public static function indexBy(callable $fn, ModelInterface ...$models): array
+    {
+        $indexed = [];
+        foreach ($models as $model) {
+            $indexed[$fn($model)] = $model;
         }
 
         return $indexed;
